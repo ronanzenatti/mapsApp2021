864886c57efb3153ac3649e21d67d354c627acad
Ronan Adriel Zenatti <ronan.zenatti@etec.sp.gov.br>
2021-09-23 06:25:21 -0300

    Maps

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,51 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild('map') mapRef: ElementRef;
+
+  map: google.maps.Map;
+  minhaPosicao: google.maps.LatLng;
+
+  constructor(private geolocation: Geolocation) { }
+
+  ionViewWillEnter() {
+    this.exibirMapa();
+  }
+
+  exibirMapa(): void {
+
+    const posicao = new google.maps.LatLng(-22.194081, -48.778859);
+    const opcoes = {
+      center: posicao,
+      zoom: 10,
+      disableDefaultUI: false
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+
+    this.buscarPosicao();
+  }
+
+  buscarPosicao() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.minhaPosicao = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+      this.irParaPosicao();
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  irParaPosicao() {
+    this.map.setCenter(this.minhaPosicao);
+    this.map.setZoom(15);
+
+    new google.maps.Marker({
+      position: this.minhaPosicao,
+      map: this.map,
+      title: 'Minha Posição!',
+      animation: google.maps.Animation.BOUNCE
+    });
+  }
 
 }
